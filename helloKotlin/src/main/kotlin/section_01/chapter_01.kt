1cfadc468a9ab87f399910cdeac193f60b646f30
Thinh Nguyen <thinh.nguyen>
2021-09-01 16:42:14 +0700

    Init

--- helloKotlin/src/main/kotlin/section_01/chapter_01.kt
+++ helloKotlin/src/main/kotlin/section_01/chapter_01.kt
@@ -1,5 +1,16 @@
 package section_01
 
+/*
+Key points:
+- IntelliJ IDEA is an Integrated Development Environment from JetBrains, the creators of the Kotlin language, in which
+you can write and run Kotlin code.
+- IntelliJ IDEA Community edition is a free version to use for the projects in the book.
+- Kotlin code runs on many platforms, and one of the most prominent is the Java Virtual Machine, which will be used for
+most of the book.
+- To build Kotlin projects with IntelliJ IDEA, you need to install the Java Development Kit, version 8 or above.
+- The IntelliJ IDEA app window consists of a number of panels, the most relevant of which are the Project panel, the
+Editor panel, and the Run panel.
+ */
 fun main() {
-    
+    println("Hello, Kotlin!")
 }
