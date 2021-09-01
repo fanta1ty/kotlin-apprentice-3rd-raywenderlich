4775a9866a84b6a3d4f324e8e0e7080dc69f932e
Thinh Nguyen <thinh.nguyen>
2021-09-01 23:22:32 +0700

    Updated

--- helloKotlin/src/main/kotlin/section_01/chapter_02.kt
+++ helloKotlin/src/main/kotlin/section_01/chapter_02.kt
@@ -1,4 +1,29 @@
 package section_01
 
+/*
+Key points:
+- Computers, at their most fundamental level, perform simple mathematics.
+- A programming language allows you to write code, which the compiler converts into instructions that the CPU can
+execute. Kotlin code on the JVM is first converted to bytecode.
+- Computers operate on numbers in base 2 form, otherwise known as binary.
+- Code comments are denoted by a line starting with // or multiple lines bookended with /* and */.
+- Code comments can be used to document your code.
+- You can use println to write things to the console area.
+- The arithmetic operators are:
+    Add: +
+    Subtract: -
+    Multiply: *
+    Divide: /
+    Remainder: %
+- Constants and variables give names to data.
+- Once you've declared a constant, you can't change its data, but you can change a variable's data at any time.
+- Always give variables and constants meaningful names to save yourself and your colleagues headaches later.
+- Operators to perform arithmetic and then assign back to the variable.
+    Add and assign: +=
+    Subtract and assign: -=
+    Multiply and assign: *=
+    Divide and assign: /=
+ */
+
 fun main() {
 }
