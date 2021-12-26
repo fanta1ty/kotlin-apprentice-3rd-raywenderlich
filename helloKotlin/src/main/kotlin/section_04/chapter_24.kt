8a526f47e7aa906d7b3a81127281db43a4865a0d
Thinh Nguyen <thinh.nguyen>
2021-12-26 23:14:54 +0700

    Updated

--- helloKotlin/src/main/kotlin/section_04/chapter_24.kt
+++ helloKotlin/src/main/kotlin/section_04/chapter_24.kt
@@ -2,6 +2,17 @@ package section_04
 
 /*
 Key points:
+- Scripting is writing small programs in a text editor that can be run from the command line and be used to do various
+types of processing on your computer.
+- As a scripting language, Kotlin gives you the static-typing lacking in other scripting languages like Python and Ruby.
+- Kotlin comes with a Read-Evaluate-Print Loop or REPL that can be used to investigate Kotlin code in an interactive
+manner.
+- Kotlin scripts end with the extension .kts, as opposed to normal Kotlin code that ends with .kt.
+- You can use Intellij IDEA as a script editor, and then either run your scripts within the IDE or from a command line
+shell on your OS.
+- Kotlin scripts run inside a hidden main() function and can access args passed into the script at the command line.
+Scripts can also import Kotlin and Java libraries to access their features.
+- You can use Kotlin scripts to read and write to the files and folders on your filesystem, and much more!
  */
 
 fun main() {
