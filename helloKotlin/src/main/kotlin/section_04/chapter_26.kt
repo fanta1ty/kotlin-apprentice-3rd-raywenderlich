4e7cf18e9ebecea8733e4fd7f818daae4eeeed49
Thinh Nguyen <thinh.nguyen>
2021-12-27 09:10:27 +0700

    Updated

--- helloKotlin/src/main/kotlin/section_04/chapter_26.kt
+++ helloKotlin/src/main/kotlin/section_04/chapter_26.kt
@@ -2,6 +2,12 @@ package section_04
 
 /*
 Key points:
+- Kotlin Multiplatform is a new and fast-growing approach to cross-platform and development.
+- KMP lets you share Kotlin code between iOS, Android, web, server, and more.
+- There are a number of advantages to KMP, including developer familiarity with Kotlin, native performance, native UI
+code, and the consolidation of your app business logic into a shared module across all platforms.
+- You use the expect and actual keywords to create a common interface within the shared code that relies on concrete
+implementations on different platforms as needed.
  */
 
 fun main() {
